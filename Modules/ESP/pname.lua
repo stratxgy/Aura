8dedfd66bac625e1a40ffad42026c990f7318f62
Stratxgy <117533771+Stratxgy@users.noreply.github.com>
2024-12-16 19:54:51 -0600

    Update pname.lua

--- Modules/ESP/pname.lua
+++ Modules/ESP/pname.lua
@@ -1,22 +1,23 @@
+
 getgenv().pname = getgenv().pname or {
-    Color = Color3.fromRGB(255, 255, 255), 
-    Enabled = false, 
-    Position = "Above", -- hi future me the options are "Above" or "Below" remember that ok? ok good.
-    Size = 10,
+    Color = Color3.fromRGB(255, 255, 255),
+    Enabled = true, 
+    Position = "Above",  -- hi future me the options are "Above" or "Below" remember that ok? ok good.
+    Size = 10, 
 }
 
 local billboards = {} 
 
 
 local function createNameBillboard(player)
-    if not getgenv().pname.Enabled then return end
     if player == game.Players.LocalPlayer then return end 
+    if not getgenv().pname.Enabled then return end 
 
     local function adornCharacter(character)
         if not getgenv().pname.Enabled then return end
         local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
-    
+
         local billboard = Instance.new("BillboardGui")
         billboard.Adornee = humanoidRootPart
         billboard.Size = UDim2.new(4, 0, 1, 0)
@@ -25,7 +26,7 @@ local function createNameBillboard(player)
             or Vector3.new(0, 3, 0)
         billboard.AlwaysOnTop = true
 
-     
+
         local textLabel = Instance.new("TextLabel")
         textLabel.Text = player.Name
         textLabel.BackgroundTransparency = 1
@@ -38,9 +39,9 @@ local function createNameBillboard(player)
         textLabel.Parent = billboard
 
         billboard.Parent = humanoidRootPart
-        billboards[player] = billboard
+        billboards[player] = billboard 
 
-  
+      
         character.AncestryChanged:Connect(function()
             if not character:IsDescendantOf(game) then
                 billboard:Destroy()
@@ -90,10 +91,8 @@ if getgenv().pname.Enabled then
     for _, player in ipairs(game.Players:GetPlayers()) do
         createNameBillboard(player)
     end
-end
 
-game.Players.PlayerAdded:Connect(function(player)
-    if getgenv().pname.Enabled then
+    game.Players.PlayerAdded:Connect(function(player)
         createNameBillboard(player)
-    end
-end)
+    end)
+end
